3a23aa6d0cd1278d2d3500c81afc3c2ba6b116fb
cph <stegben.benjamin@gmail.com>
2018-12-03 20:38:52 +0800

    v0.2.1

--- setup.py
+++ setup.py
@@ -1,16 +1,25 @@
+from pathlib import Path
 from setuptools import setup, find_packages
 
-try:
-    long_description = open("README.md").read()
-except IOError:
-    long_description = ""
+readme = Path(__file__).parent.joinpath('README.md')
+if readme.exists():
+    with readme.open() as f:
+        long_description = f.read()
+        try:
+            from pypandoc import convert_text
+            long_description = convert_text(
+                long_description, 'rst', format='md')
+        except ImportError:
+            print("warning: pypandoc module not found, could not convert Markdown to RST")
+else:
+    long_description = '-'
 
 REQUIRED_PACKAGES = []
 
 
 setup(
     name="aiomessenger",
-    version="0.2.0",
+    version="0.2.1",
     description=long_description,
     author="cph",
     author_email="stegben.benjamin@gmail.com",
